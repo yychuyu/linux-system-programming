6341d35e83100af37c8a57d3e2f420d76a909e15
Alvin Yan <yychuyu@163.com>
2018-05-03 20:49:08 +0800

    add orphan, zombie

---
+++ wait/orphan.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+    pid_t pid;
+    pid = fork();
+    if (pid == 0) {
+        while (1) {
+			/* 子进程死循环，保证不死 */
+            printf("I am child, my parent pid = %d\n", getppid());
+            sleep(1);
+        }
+    } else if (pid > 0) {
+            printf("I am parent, my pid is = %d\n", getpid());
+            sleep(3);
+            printf("------------parent going to die------------\n");
+    } else {
+        perror("fork");
+        return 1;
+    }
+    return 0;
+}
